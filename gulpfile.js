7a93156783f23bf1def065773c5d318d2a0e551f
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2016-10-24 22:22:44 -0700

    Add browsersync & watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
+// http://flexbox.io/
+// https://css-tricks.com/gulp-for-beginners/
+
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
     // source files
@@ -11,7 +15,25 @@ gulp.task('styles', function() {
 
 gulp.task('sass', function() {
     // source files
-    gulp.src('app/sass/**/*.sass')
+    gulp.src('app/scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    });
+});
+
+gulp.task('watch', ['browserSync'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/css/*.css', ['styles']);
+    gulp.watch('app/*.html', browserSync.reload);
+    // gulp.watch('app/js/**/*.js', browserSync.reload);
 });
